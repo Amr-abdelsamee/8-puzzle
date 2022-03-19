4f6f066eed02c1e3f1102321a8d436fa4e72c6e3
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-20 00:58:23 +0200

    search for empty
    check_empty_near function is added it checks if the clicked block can be transferred to the empty block

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import math
 import pygame
 from blocks import block
 import math
+import random
 def main_menu():
     while True:
         pass
@@ -44,19 +45,77 @@ x = SIDES_PADDING
 y = UPPER_PADDING
 
 blocks = []
-text_filler_str = ""
-text_filler_num = 0
+
+labels = random.sample(range(NUM_BLOCKS), NUM_BLOCKS)
+position = 0
+empty_block_index = 0
 # create rect
 for i in range(0, NUM_ROW_COL):
     for j in range(0, NUM_ROW_COL):
-        rec = block(x, y, BLOCK_COLOR, text_filler_str, text_filler_num, WHITE, FONT, BLOCK_WIDTH, BLOCK_HEIGHT)
+        rec = block(x, y, BLOCK_COLOR, labels[position], position, WHITE, FONT, BLOCK_WIDTH, BLOCK_HEIGHT)
         rec.draw(screen)
         blocks.append(rec)
-        text_filler_num += 1
-        text_filler_str = str(text_filler_num)
+        # storing the index of the empty block index
+        if labels[position]==0: 
+            empty_block_index = position 
+            print(empty_block_index)
         x = x + BLOCK_WIDTH + INBTWN_SPACE
+        position += 1
     x = SIDES_PADDING
     y = y + BLOCK_HEIGHT + INBTWN_SPACE
+
+def check_empty_near(clicked_index):
+    # check if in upper left corners
+    if clicked_index % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index + NUM_ROW_COL].get_label()):
+            return True
+    # check if in upper right corners
+    elif (clicked_index + 1) % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
+        if( not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index + NUM_ROW_COL].get_label()):
+            return True
+    # check if in lower left corners
+    elif clicked_index % NUM_ROW_COL == 0 and clicked_index >= len(blocks) - NUM_ROW_COL:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index - NUM_ROW_COL].get_label()):
+            return True
+    # check if in lower right corners
+    elif (clicked_index + 1) % NUM_ROW_COL == 0 and clicked_index >= len(blocks) - NUM_ROW_COL:
+        if( not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index - NUM_ROW_COL].get_label()):
+            return True
+    # check if the clicked block is in first row
+    elif clicked_index < NUM_ROW_COL:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index + NUM_ROW_COL].get_label()):
+            return True
+    # check if the clicked block is in last row
+    elif clicked_index >= len(blocks) - NUM_ROW_COL:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index - NUM_ROW_COL].get_label()):
+            return True
+    # check if the clicked block is in first col
+    elif clicked_index % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index + NUM_ROW_COL].get_label()):
+            return True
+    # check if the clicked block is in last col
+    elif clicked_index + 1 % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index - NUM_ROW_COL].get_label()):
+            return True
+    # any other place in the middle
+    else:
+        if( not blocks[clicked_index + 1].get_label()
+        or not blocks[clicked_index - 1].get_label()
+        or not blocks[clicked_index + NUM_ROW_COL].get_label()
+        or not blocks[clicked_index - NUM_ROW_COL].get_label()):
+            return True
 
 
 # running loop
@@ -67,11 +126,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            
             x_clicked,y_clicked = pygame.mouse.get_pos()
             for i in range(len(blocks)):
+                
                 if blocks[i].check_clicked(x_clicked, y_clicked):
+                    if not blocks[i].get_label():
+                        continue
+                    clicked_index = i
+                    print("can move to the empty: " + str(check_empty_near(i)))
+                    print("clicked inedx: "+str(clicked_index))
                     print(pygame.mouse.get_pos())
-                    print(blocks[i].get_position())
+                    print("get_label: "+str(blocks[i].get_label())+"\n")
         
         pygame.display.update()
 
