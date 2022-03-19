2d06f206fd0f489c6850a9d89d1e8cce0cd29aec
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-19 23:10:29 +0200

    mouse action added

--- blocks.py
+++ blocks.py
@@ -1,12 +1,14 @@
 import pygame
 
 class block:
-    def __init__(self, x_pos, y_pos, color, text, text_color, FONT, width, height):
+    def __init__(self, x_pos, y_pos, color, text, position, text_color, FONT, width, height):
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.color = color
-        self.text_color = text_color
         self.text_in = text
+        self.position = position
+        self.text_color = text_color
+        
         self.text = FONT.render(self.text_in, True, self.text_color)
         self.width = width
         self.height = height
@@ -17,3 +19,11 @@ class block:
         # screen.blit(self.text, (self.x_pos+int(self.size/2), self.y_pos+int(self.size/2)))
         screen.blit(self.text, (self.x_pos, self.y_pos))
         
+    def check_clicked(self, x_clicked , y_clicked):
+        if( x_clicked >= self.x_pos 
+        and x_clicked < self.x_pos + self.width
+        and y_clicked >= self.y_pos
+        and y_clicked < self.y_pos + self.height):
+            return True
+    def get_position(self):
+        return self.position
--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def main_menu():
 pygame.init()
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = SCREEN_WIDTH
-FONT_SIZE = 80
-FONT = pygame.font.SysFont('cambria', FONT_SIZE)
+
 RED = (255,0,0)
 WHITE = (255, 255, 255)
 
@@ -24,11 +23,12 @@ NUM_ROW_COL = int(math.sqrt(NUM_BLOCKS))
 
 BLOCK_WIDTH = (SCREEN_WIDTH - (2*SIDES_PADDING) - (NUM_ROW_COL*INBTWN_SPACE-1)) / NUM_ROW_COL
 BLOCK_HEIGHT = (SCREEN_WIDTH - UPPER_PADDING - LOWER_PADDING - (NUM_ROW_COL*INBTWN_SPACE-1)) / NUM_ROW_COL
-
+BLOCK_COLOR = RED
 #create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
+FONT_SIZE = int(0.5*BLOCK_WIDTH)
+FONT = pygame.font.SysFont('cambria', FONT_SIZE)
 
 #title & logo
 pygame.display.set_caption("8-puzzle")
@@ -43,15 +43,17 @@ screen.fill(WHITE)
 x = SIDES_PADDING
 y = UPPER_PADDING
 
+blocks = []
 text_filler_str = ""
 text_filler_num = 0
 # create rect
 for i in range(0, NUM_ROW_COL):
     for j in range(0, NUM_ROW_COL):
-        rec = block(x, y, RED, text_filler_str, WHITE, FONT, BLOCK_WIDTH, BLOCK_HEIGHT)
+        rec = block(x, y, BLOCK_COLOR, text_filler_str, text_filler_num, WHITE, FONT, BLOCK_WIDTH, BLOCK_HEIGHT)
         rec.draw(screen)
+        blocks.append(rec)
         text_filler_num += 1
-        temp = str(text_filler_num)
+        text_filler_str = str(text_filler_num)
         x = x + BLOCK_WIDTH + INBTWN_SPACE
     x = SIDES_PADDING
     y = y + BLOCK_HEIGHT + INBTWN_SPACE
@@ -65,9 +67,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-    pygame.display.update()
+            x_clicked,y_clicked = pygame.mouse.get_pos()
+            for i in range(len(blocks)):
+                if blocks[i].check_clicked(x_clicked, y_clicked):
+                    print(pygame.mouse.get_pos())
+                    print(blocks[i].get_position())
+        
+        pygame.display.update()
 
 pygame.quit()
 
