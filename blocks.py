1b47395698f4aa271cc887248b125f67c7716608
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-19 21:28:25 +0200

    dynamic blocks creation

--- blocks.py
+++ blocks.py
@@ -1,17 +1,18 @@
 import pygame
 
 class block:
-    def __init__(self, x_pos, y_pos, color, text, text_color, FONT, size):
+    def __init__(self, x_pos, y_pos, color, text, text_color, FONT, width, height):
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.color = color
         self.text_color = text_color
         self.text_in = text
         self.text = FONT.render(self.text_in, True, self.text_color)
-        self.size = size
+        self.width = width
+        self.height = height
         
     def draw(self,screen):
-        pygame.draw.rect(screen,self.color, (self.x_pos, self.y_pos, self.size, self.size))
+        pygame.draw.rect(screen,self.color, (self.x_pos, self.y_pos, self.width, self.height))
 
         # screen.blit(self.text, (self.x_pos+int(self.size/2), self.y_pos+int(self.size/2)))
         screen.blit(self.text, (self.x_pos, self.y_pos))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import math
 import pygame
 from blocks import block
-
+import math
 def main_menu():
     while True:
         pass
@@ -8,12 +9,22 @@ def main_menu():
 # initializing the pygame
 pygame.init()
 SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = SCREEN_WIDTH
 FONT_SIZE = 80
 FONT = pygame.font.SysFont('cambria', FONT_SIZE)
 RED = (255,0,0)
 WHITE = (255, 255, 255)
-BLOCK_SIZE = 150
+
+SIDES_PADDING = 10
+UPPER_PADDING = 100
+LOWER_PADDING = 20
+INBTWN_SPACE = 1
+NUM_BLOCKS = 9
+NUM_ROW_COL = int(math.sqrt(NUM_BLOCKS))
+
+BLOCK_WIDTH = (SCREEN_WIDTH - (2*SIDES_PADDING) - (NUM_ROW_COL*INBTWN_SPACE-1)) / NUM_ROW_COL
+BLOCK_HEIGHT = (SCREEN_WIDTH - UPPER_PADDING - LOWER_PADDING - (NUM_ROW_COL*INBTWN_SPACE-1)) / NUM_ROW_COL
+
 #create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -27,29 +38,35 @@ pygame.display.set_icon(icon)
 #background color
 screen.fill(WHITE)
 
+
+#initial cordinates of the first block
+x = SIDES_PADDING
+y = UPPER_PADDING
+
+text_filler_str = ""
+text_filler_num = 0
 # create rect
-x = 65
-y = 100
-temp = ""
-num_filler = 0
-space = BLOCK_SIZE + 10
-for i in range(0, 3):
-    for j in range(0, 3):
-        rec = block(x, y, RED, temp, WHITE,FONT, BLOCK_SIZE)
+for i in range(0, NUM_ROW_COL):
+    for j in range(0, NUM_ROW_COL):
+        rec = block(x, y, RED, text_filler_str, WHITE, FONT, BLOCK_WIDTH, BLOCK_HEIGHT)
         rec.draw(screen)
-        num_filler += 1
-        temp = str(num_filler)
-        x += space
-    x = 65
-    y += space
+        text_filler_num += 1
+        temp = str(text_filler_num)
+        x = x + BLOCK_WIDTH + INBTWN_SPACE
+    x = SIDES_PADDING
+    y = y + BLOCK_HEIGHT + INBTWN_SPACE
 
 
 # running loop
 running = True
 while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
     pygame.display.update()
 
 pygame.quit()
