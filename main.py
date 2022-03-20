8919f1af2e64ef9de0eb27943fc9365fe12acbfc
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-20 02:51:03 +0200

    movement added
    function exchange added it takes the clicked block index and exchange its place with the empty block place
    
    Also, numbers now are generated in random places

--- main.py
+++ main.py
@@ -25,11 +25,12 @@ NUM_ROW_COL = int(math.sqrt(NUM_BLOCKS))
 BLOCK_WIDTH = (SCREEN_WIDTH - (2*SIDES_PADDING) - (NUM_ROW_COL*INBTWN_SPACE-1)) / NUM_ROW_COL
 BLOCK_HEIGHT = (SCREEN_WIDTH - UPPER_PADDING - LOWER_PADDING - (NUM_ROW_COL*INBTWN_SPACE-1)) / NUM_ROW_COL
 BLOCK_COLOR = RED
+FONT_SIZE = int(0.5*BLOCK_WIDTH)
+FONT = pygame.font.SysFont('cambria', FONT_SIZE)
+
 #create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-FONT_SIZE = int(0.5*BLOCK_WIDTH)
-FONT = pygame.font.SysFont('cambria', FONT_SIZE)
 
 #title & logo
 pygame.display.set_caption("8-puzzle")
@@ -43,6 +44,7 @@ screen.fill(WHITE)
 #initial cordinates of the first block
 x = SIDES_PADDING
 y = UPPER_PADDING
+
 
 blocks = []
 
@@ -64,58 +66,84 @@ for i in range(0, NUM_ROW_COL):
     x = SIDES_PADDING
     y = y + BLOCK_HEIGHT + INBTWN_SPACE
 
+def exchange(clicked_index):
+    global  empty_block_index
+    motion_speed = 1
+    
+    old_x = blocks[clicked_index].get_x_pos()
+    new_x = blocks[empty_block_index].get_x_pos()
+    old_y = blocks[clicked_index].get_y_pos()
+    new_y = blocks[empty_block_index].get_y_pos()
+
+    blocks[clicked_index].set_x_pos(new_x)
+    blocks[clicked_index].set_y_pos(new_y)
+    blocks[clicked_index].update()
+    blocks[clicked_index].draw(screen)
+
+    blocks[empty_block_index].set_x_pos(old_x)
+    blocks[empty_block_index].set_y_pos(old_y)
+    blocks[empty_block_index].update()
+    blocks[empty_block_index].draw(screen)
+
+    temp = blocks[clicked_index]
+    blocks[clicked_index] = blocks[empty_block_index]
+    blocks[empty_block_index] = temp
+    
+    empty_block_index = clicked_index
+
+
 def check_empty_near(clicked_index):
     # check if in upper left corners
     if clicked_index % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index + NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if in upper right corners
     elif (clicked_index + 1) % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
         if( not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index + NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if in lower left corners
     elif clicked_index % NUM_ROW_COL == 0 and clicked_index >= len(blocks) - NUM_ROW_COL:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index - NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if in lower right corners
     elif (clicked_index + 1) % NUM_ROW_COL == 0 and clicked_index >= len(blocks) - NUM_ROW_COL:
         if( not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index - NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if the clicked block is in first row
     elif clicked_index < NUM_ROW_COL:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index + NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if the clicked block is in last row
     elif clicked_index >= len(blocks) - NUM_ROW_COL:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index - NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if the clicked block is in first col
     elif clicked_index % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index + NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # check if the clicked block is in last col
     elif clicked_index + 1 % NUM_ROW_COL == 0 and clicked_index < NUM_ROW_COL:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index - NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
     # any other place in the middle
     else:
         if( not blocks[clicked_index + 1].get_label()
         or not blocks[clicked_index - 1].get_label()
         or not blocks[clicked_index + NUM_ROW_COL].get_label()
         or not blocks[clicked_index - NUM_ROW_COL].get_label()):
-            return True
+            exchange(clicked_index)
 
 
 # running loop
@@ -134,7 +162,7 @@ while running:
                     if not blocks[i].get_label():
                         continue
                     clicked_index = i
-                    print("can move to the empty: " + str(check_empty_near(i)))
+                    check_empty_near(i)
                     print("clicked inedx: "+str(clicked_index))
                     print(pygame.mouse.get_pos())
                     print("get_label: "+str(blocks[i].get_label())+"\n")
