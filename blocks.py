47d9374bdb0fb05f8fa7375d3d1ae8ee0d2acfc2
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-03-20 02:52:43 +0200

    more function are added
    getters and setters are added for certain props
    function update is added to update the coordinates  with the new values

--- blocks.py
+++ blocks.py
@@ -9,17 +9,20 @@ class block:
         else:self.label = str(label)
         self.position = position
         self.text_color = text_color
-        
-        self.text = FONT.render(self.label, True, self.text_color)
         self.width = width
         self.height = height
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
+        self.text = FONT.render(self.label, True, self.text_color)
         
-    def draw(self,screen):
-        pygame.draw.rect(screen,self.color, (self.x_pos, self.y_pos, self.width, self.height))
-
+        
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
         # screen.blit(self.text, (self.x_pos+int(self.size/2), self.y_pos+int(self.size/2)))
         screen.blit(self.text, (self.x_pos, self.y_pos))
-        
+    
+    def update(self,):
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
+
     def check_clicked(self, x_clicked , y_clicked):
         if( x_clicked >= self.x_pos 
         and x_clicked < self.x_pos + self.width
@@ -32,3 +35,18 @@ class block:
     
     def get_position(self):
         return self.position
+
+    def get_x_pos(self):
+        return self.x_pos
+    
+    def get_y_pos(self):
+        return self.y_pos
+
+    def set_x_pos(self, new_x_pos):
+        self.x_pos = new_x_pos
+    
+    def set_y_pos(self, new_y_pos):
+        self.y_pos = new_y_pos
+    
+    def set_position(self, new_position):
+        self.position = new_position
