2ea78232e4464dcf4013fbe92afd1a4b69dbe35e
Mohamed Saeed Abdelhafez <mohammed1832001@hotmail.com>
2022-03-25 18:33:03 +0200

    agents commented
    main has variable delay.

--- agents.py
+++ agents.py
@@ -3,107 +3,112 @@ import random
 from collections import deque
 
 
-class hashmap:
+# Creating hash map class to add explored nodes in it
+class Hashmap:
+    # initialize with size and create table
     def __init__(self, size) -> None:
         self.size = size
         self.hash_table = self.create_table()
 
+    # Create empty table
     def create_table(self):
         return [[] for _ in range(self.size)]
 
-    def Hashing(self, keyvalue):
+    # Function to get key
+    def hashing(self, keyvalue):
         return keyvalue % self.size
 
+    # Insert to hash table
     def insert(self, value):
-        hash_key = self.Hashing(value)
+        hash_key = self.hashing(value)
         self.hash_table[hash_key].append(value)
 
+    # Find if element is in hash table
     def search(self, val):
-        hash_key = self.Hashing(val)
+        hash_key = self.hashing(val)
         for i in self.hash_table[hash_key]:
             if i == val:
                 return True
         return False
 
 
+# Class Node with all required node data to form a tree
 class Node:
     def __init__(self, parent, index, state, empty_index, cost=0, current_cost=0, depth=0):
-        self.state = state
-        self.parent = parent
-        self.index = index
-        self.empty_index = empty_index
-        self.cost = cost
-        self.current_cost = current_cost
-        self.depth = depth
+        self.state = state  # State in string form
+        self.parent = parent  # Parent index in tree array
+        self.index = index  # Node index in tree
+        self.empty_index = empty_index  # The zero index in the state
+        self.cost = cost  # Cost to be used by A*
+        self.current_cost = current_cost  # Actual cost to reach this node (Depth)
 
 
+# Class Agents which all other agents inherit from
 class Agents:
     def __init__(self, start_state, empty_index, solution, movable):
-        self.solution = solution
-        self.movable = movable
-        self.root = Node(0, 0, start_state, empty_index)
-        self.current_node = Node(0, 0, start_state, empty_index)
-        self.tree = []
-        self.tree.append(self.root)
-        self.explored = hashmap(10000)
-        self.max_depth = 0
+        self.solution = solution  # Solution is set to be changeable
+        self.movable = movable  # Movable array of arrays for each index of zero
+        self.root = Node(0, 0, start_state, empty_index)  # Initial state node
+        self.current_node = Node(0, 0, start_state, empty_index)  # First node to be used
+        self.tree = []  # Empty tree array
+        self.tree.append(self.root)  # Append the root to tree
+        self.explored = Hashmap(10000)  # Creating explored as hashmap object
+        self.max_depth = 0  # Depth of deepest leaf
 
+    # Check if a node with this state is already created
     def contains(self, state):
         return self.explored.search(int(state))
 
+    # Method to create children of current node
     def next_states(self, current_node):
-        new_nodes = []
-        random.shuffle(self.movable[current_node.empty_index])
+        new_nodes = []  # Array to hold new nodes
+        random.shuffle(self.movable[current_node.empty_index])  # Randomize which index chooses to replace with first
         for i in self.movable[current_node.empty_index]:
-            state = list(current_node.state)
+            state = list(current_node.state)  # List of current state to be swapped
             state[i], state[current_node.empty_index] = state[current_node.empty_index], state[i]
-            state = ''.join(state)
+            state = ''.join(state)  # Restore state to string
             if self.contains(state):
-                pass
-                # print('repeated')
-                # print(state)
-                # print('...')
+                pass  # If state is existing do nothing
             else:
-                self.explored.insert(int(state))
+                self.explored.insert(int(state))  # Insert state in explored then create node ti
                 new_node = Node(current_node.index, len(self.tree), state, i,
-                                current_cost=current_node.current_cost + 1, depth=current_node.depth + 1)
-                if new_node.depth > self.max_depth:
-                    self.max_depth = new_node.depth
-                self.tree.append(new_node)
+                                current_cost=current_node.current_cost + 1)
+                if new_node.current_cost > self.max_depth:
+                    self.max_depth = new_node.current_cost
+                self.tree.append(new_node)  # Add node to tree
                 new_nodes.append(new_node)
         return new_nodes
 
+    # Function to check if goal reached
     def check_goal(self):
         if np.array_equal(self.current_node.state, self.solution):
             return True
         return False
 
 
+# DFS Agent
 class DFS(Agents):
+    # Add frontier to initialization
     def __init__(self, start_state, empty_index, solution, movable):
         super().__init__(start_state, empty_index, solution, movable)
         self.frontier = []
 
+    # Add to frontier
     def add(self, node):
         self.frontier.append(node)
 
-    def empty(self):
-        print(len(self.frontier))
-        if len(self.frontier) == 0:
-            return False
-        return True
-
-    def deq(self):
-        # if self.empty():
-        #     raise Exception("kosom da brnamg")
+    # Pop from frontier
+    def pop(self):
         node = self.frontier.pop()
         return node
 
+    # Update method to get children of node
     def update(self):
         new_nodes = self.next_states(self.current_node)
         for i in new_nodes:
             self.add(i)
 
+    # Method to get path by back tracking after reaching goal
     def get_path(self):
         node = self.current_node
         path = []
@@ -112,14 +117,12 @@ class DFS(Agents):
             node = self.tree[node.parent]
         return path
 
+    # Work function to run the agent
     def work(self):
-        # points = 1
         while not self.check_goal():
             self.update()
-            node = self.deq()
+            node = self.pop()
             self.current_node = node
-            # print(self.current_node.state, ' ', points)
-            # points +=1
 
         path = self.get_path()
         path.reverse()
@@ -128,31 +131,27 @@ class DFS(Agents):
 
 
 class BFS(Agents):
-
+    # Add frontier to initialization
     def __init__(self, start_state, empty_index, solution, movable):
         super().__init__(start_state, empty_index, solution, movable)
         self.frontier = deque()
 
+    # Add to frontier
     def add(self, node):
         self.frontier.append(node)
 
-    def empty(self):
-        print(len(self.frontier))
-        if len(self.frontier) == 0:
-            return False
-        return True
-
+    # Deque from frontier
     def deq(self):
-        # if self.empty():
-        #     raise Exception("kosom da brnamg")
         node = self.frontier.popleft()
         return node
 
+    # Update method to get children of node
     def update(self):
         new_nodes = self.next_states(self.current_node)
         for i in new_nodes:
             self.add(i)
 
+    # Method to get path by back tracking after reaching goal
     def get_path(self):
         node = self.current_node
         path = []
@@ -161,6 +160,7 @@ class BFS(Agents):
             node = self.tree[node.parent]
         return path
 
+    # Work function to run the agent
     def work(self):
         # points = 0
         self.add(self.current_node)
@@ -177,7 +177,8 @@ class BFS(Agents):
         return path, moves, self.max_depth, len(self.tree)
 
 
-class PriorityQueue():
+# Priority Queue class to be used by A* agent
+class PriorityQueue:
     def __init__(self):
         self.queue = []
 
@@ -189,7 +190,7 @@ class PriorityQueue():
     def enter(self, node):
         self.queue.append(node)
 
-    # for popping an element based on Priority
+    # for popping an element based on Priority (Lowest Cost)
     def pop(self):
         try:
             min = 0
@@ -203,6 +204,7 @@ class PriorityQueue():
             print()
 
 
+# Convert String state to integer array
 def string_to_int(array):
     array = list(array)
     array = ' '.join(array)
@@ -211,29 +213,29 @@ def string_to_int(array):
 
 
 class AStar(Agents):
-    def __init__(self, start_state, empty_index, solution,movable, type):
+    # Add frontier to initialization and type of heuristic
+    def __init__(self, start_state, empty_index, solution, movable, type):
         super().__init__(start_state, empty_index, solution, movable)
         self.frontier = PriorityQueue()
         self.type = type
 
+    # Add to frontier
     def add(self, node):
         node.cost = self.heu(node.state, self.type) + self.current_node.current_cost
         self.frontier.enter(node)
 
-    def empty(self):
-        return self.frontier.isEmpty()
-
-    def deq(self):
-        if self.empty():
-            raise Exception("kosom da brnamg")
+    # Pop from frontier
+    def pop(self):
         node = self.frontier.pop()
         return node
 
+    # Update method to get children of node
     def update(self):
         new_nodes = self.next_states(self.current_node)
         for i in new_nodes:
             self.add(i)
 
+    # Method to get path by back tracking after reaching goal
     def get_path(self):
         node = self.current_node
         path = []
@@ -242,11 +244,12 @@ class AStar(Agents):
             node = self.tree[node.parent]
         return path
 
+    # Work function to run the agent
     def work(self):
         # points = 0
         self.add(self.current_node)
         while not self.check_goal():
-            node = self.deq()
+            node = self.pop()
             self.current_node = node
             self.update()
             # print(self.current_node.state, ' ', points)
@@ -256,13 +259,11 @@ class AStar(Agents):
         moves = len(path)
         return path, moves, self.max_depth, len(self.tree)
 
+    # Method calculates heuristics for a given state
     def heu(self, state, type):
-        """
-        calculates heuristics for a given state
-        """
         state = string_to_int(state)
         sum = 0
-        if type == 1:
+        if type == 1:           # Manhattan distance
             for i in range(3):
                 for j in range(3):
                     num = state[i * 3 + j]
@@ -271,7 +272,7 @@ class AStar(Agents):
                     x = (num % 3) - j
                     y = (num // 3) - i
                     sum += abs(x) + abs(y)
-        else:
+        else:               # Euclidean distance
             for i in range(3):
                 for j in range(3):
                     num = state[i * 3 + j]
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from copy import copy
 from buttons import Button
 import time
 
-
 NUM_BLOCKS = 9
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = SCREEN_WIDTH
@@ -14,9 +13,9 @@ BUTTON_WIDTH = 400
 BUTTON_HEIGHT = 100
 SIDES_PADDING = 10
 INBTWN_SPACE = 1
-PUZZLE_WIDTH = (SCREEN_WIDTH - (2*SIDES_PADDING))
-BACK_GRND_COLOR = (255,255,255)
-RED = (123,44,130)
+PUZZLE_WIDTH = (SCREEN_WIDTH - (2 * SIDES_PADDING))
+BACK_GRND_COLOR = (255, 255, 255)
+RED = (123, 44, 130)
 bg = pygame.image.load("BG.png")
 start_player = False
 agent_selected = None
@@ -26,24 +25,28 @@ font_size = 50
 text_size = 30
 padding = 4
 
+
 def start_window():
     global start_player
     buttons = []
-    player_button = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"Play",BACK_GRND_COLOR,font_size)
+    player_button = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2), (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100,
+                           BUTTON_WIDTH, BUTTON_HEIGHT, RED, "Play", BACK_GRND_COLOR, font_size)
     player_button.draw(game_screen)
     buttons.append(player_button)
-    AI_button = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100 + BUTTON_HEIGHT + padding, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"AI",BACK_GRND_COLOR,font_size)
+    AI_button = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2),
+                       (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100 + BUTTON_HEIGHT + padding, BUTTON_WIDTH,
+                       BUTTON_HEIGHT, RED, "AI", BACK_GRND_COLOR, font_size)
     AI_button.draw(game_screen)
     buttons.append(AI_button)
-    
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x_clicked,y_clicked = pygame.mouse.get_pos()
-                
+                x_clicked, y_clicked = pygame.mouse.get_pos()
+
                 for i in range(len(buttons)):
                     if buttons[i].check_clicked(x_clicked, y_clicked):
                         if i == 0:
@@ -51,31 +54,37 @@ def start_window():
                             return
                         if i == 1:
                             return
-                
+
             pygame.display.update()
     pygame.quit()
 
+
 def AI_window():
-    game_screen.blit(bg,(0,0))
+    game_screen.blit(bg, (0, 0))
     global agent_selected
     buttons = []
-    BFS = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"BFS",BACK_GRND_COLOR,font_size)
+    BFS = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2), (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100,
+                 BUTTON_WIDTH, BUTTON_HEIGHT, RED, "BFS", BACK_GRND_COLOR, font_size)
     BFS.draw(game_screen)
     buttons.append(BFS)
-    DFS = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100 + BUTTON_HEIGHT + padding, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"DFS",BACK_GRND_COLOR,font_size)
+    DFS = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2),
+                 (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100 + BUTTON_HEIGHT + padding, BUTTON_WIDTH,
+                 BUTTON_HEIGHT, RED, "DFS", BACK_GRND_COLOR, font_size)
     DFS.draw(game_screen)
     buttons.append(DFS)
-    a_star = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100 + (2*BUTTON_HEIGHT) + (2*padding), BUTTON_WIDTH,BUTTON_HEIGHT,RED,"A*",BACK_GRND_COLOR,font_size)
+    a_star = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2),
+                    (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100 + (2 * BUTTON_HEIGHT) + (2 * padding),
+                    BUTTON_WIDTH, BUTTON_HEIGHT, RED, "A*", BACK_GRND_COLOR, font_size)
     a_star.draw(game_screen)
     buttons.append(a_star)
-    
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x_clicked,y_clicked = pygame.mouse.get_pos()
+                x_clicked, y_clicked = pygame.mouse.get_pos()
                 for i in range(len(buttons)):
                     if buttons[i].check_clicked(x_clicked, y_clicked):
                         if i == 0:
@@ -87,30 +96,36 @@ def AI_window():
                         if i == 2:
                             agent_selected = 2
                             return
-                
+
             pygame.display.update()
     pygame.quit()
 
+
 def generating_window():
-    game_screen.blit(bg,(0,0))
+    game_screen.blit(bg, (0, 0))
     global generate_random
     buttons = []
-    text = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"Generate the puzzle random?",BACK_GRND_COLOR,text_size)
+    text = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2), (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100,
+                  BUTTON_WIDTH, BUTTON_HEIGHT, RED, "Generate the puzzle random?", BACK_GRND_COLOR, text_size)
     text.draw(game_screen)
-    YES = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100 + (BUTTON_HEIGHT)+padding, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"YES",BACK_GRND_COLOR,font_size)
+    YES = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2),
+                 (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100 + (BUTTON_HEIGHT) + padding, BUTTON_WIDTH,
+                 BUTTON_HEIGHT, RED, "YES", BACK_GRND_COLOR, font_size)
     YES.draw(game_screen)
     buttons.append(YES)
-    NO = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100 + (2*BUTTON_HEIGHT)+(2*padding), BUTTON_WIDTH,BUTTON_HEIGHT,RED,"NO",BACK_GRND_COLOR,font_size)
+    NO = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2),
+                (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100 + (2 * BUTTON_HEIGHT) + (2 * padding), BUTTON_WIDTH,
+                BUTTON_HEIGHT, RED, "NO", BACK_GRND_COLOR, font_size)
     NO.draw(game_screen)
     buttons.append(NO)
-    
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x_clicked,y_clicked = pygame.mouse.get_pos()
+                x_clicked, y_clicked = pygame.mouse.get_pos()
                 for i in range(len(buttons)):
                     if buttons[i].check_clicked(x_clicked, y_clicked):
                         if i == 0:
@@ -118,29 +133,33 @@ def generating_window():
                             return
                         if i == 1:
                             return
-                
+
             pygame.display.update()
     pygame.quit()
 
+
 def A_Star_window():
-    game_screen.blit(bg,(0,0))
+    game_screen.blit(bg, (0, 0))
     global A_star_type
     buttons = []
-    mnha_type = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"Manhaten Type",BACK_GRND_COLOR,text_size)
+    mnha_type = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2), (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100,
+                       BUTTON_WIDTH, BUTTON_HEIGHT, RED, "Manhaten Type", BACK_GRND_COLOR, text_size)
     mnha_type.draw(game_screen)
     buttons.append(mnha_type)
 
-    eqlid_type = Button((SCREEN_WIDTH//2)-(BUTTON_WIDTH//2), (SCREEN_HEIGHT//2)-(BUTTON_HEIGHT//2)-100 + BUTTON_HEIGHT + padding, BUTTON_WIDTH,BUTTON_HEIGHT,RED,"Ecledian Type",BACK_GRND_COLOR,text_size)
+    eqlid_type = Button((SCREEN_WIDTH // 2) - (BUTTON_WIDTH // 2),
+                        (SCREEN_HEIGHT // 2) - (BUTTON_HEIGHT // 2) - 100 + BUTTON_HEIGHT + padding, BUTTON_WIDTH,
+                        BUTTON_HEIGHT, RED, "Ecledian Type", BACK_GRND_COLOR, text_size)
     eqlid_type.draw(game_screen)
     buttons.append(eqlid_type)
-    
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x_clicked,y_clicked = pygame.mouse.get_pos()
+                x_clicked, y_clicked = pygame.mouse.get_pos()
                 for i in range(len(buttons)):
                     if buttons[i].check_clicked(x_clicked, y_clicked):
                         if i == 0:
@@ -149,26 +168,23 @@ def A_Star_window():
                         if i == 1:
                             A_star_type = 0
                             return
-                
+
             pygame.display.update()
     pygame.quit()
+
 
 pygame.init()
 game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 bg = pygame.image.load("BG.png")
-game_screen.blit(bg,(0,0))
+game_screen.blit(bg, (0, 0))
 pygame.display.set_caption("8-puzzle")
 icon = pygame.image.load('logo.png')
 pygame.display.set_icon(icon)
-
-
 
 pygame.display.update()
 
 # 125304678
 SOLUTION = '012345678'
-
-
 
 # running loop
 
@@ -177,9 +193,9 @@ start_window()
 
 if start_player:
     # generating_window()
-    game_screen.blit(bg,(0,0))
+    game_screen.blit(bg, (0, 0))
     puzzle = Puzzle(NUM_BLOCKS, game_screen)
-    
+
     running = True
     solved = False
     while running:
@@ -197,8 +213,9 @@ if start_player:
                         puzzle.check_empty_near(i)
                         solved = puzzle.check_solved()
                         if solved:
-                            witdth = (PUZZLE_WIDTH-(2*INBTWN_SPACE))//3
-                            win_text = Button(SIDES_PADDING+ witdth+INBTWN_SPACE, 10, witdth, BUTTON_HEIGHT-60, RED," Game Solved! ",BACK_GRND_COLOR,text_size)
+                            witdth = (PUZZLE_WIDTH - (2 * INBTWN_SPACE)) // 3
+                            win_text = Button(SIDES_PADDING + witdth + INBTWN_SPACE, 10, witdth, BUTTON_HEIGHT - 60,
+                                              RED, " Game Solved! ", BACK_GRND_COLOR, text_size)
                             win_text.draw(puzzle.screen)
                         # print(valid_moves)
             pygame.display.update()
@@ -207,14 +224,12 @@ if start_player:
 
 else:
     AI_window()
-    game_screen.blit(bg,(0,0))
+    game_screen.blit(bg, (0, 0))
     puzzle = None
-    
-    
-    
+
     # generating_window()game_screen.blit(bg,(0,0))
     if agent_selected != None:
-        
+
         if agent_selected == 0:
             puzzle = Puzzle(NUM_BLOCKS, game_screen)
             labels = ''.join(map(str, puzzle.labels))
@@ -225,35 +240,39 @@ else:
             agent = DFS(labels, puzzle.empty_block_index, SOLUTION, puzzle.valid_moves)
         elif agent_selected == 2:
             A_Star_window()
-            game_screen.blit(bg,(0,0))
+            game_screen.blit(bg, (0, 0))
             puzzle = Puzzle(NUM_BLOCKS, game_screen)
             labels = ''.join(map(str, puzzle.labels))
             agent = AStar(labels, puzzle.empty_block_index, SOLUTION, puzzle.valid_moves, A_star_type)
-    
+
     start = time.time()
     path, moves, depth, no_explored = agent.work()
     end = time.time()
 
     time_taken = end - start
-    print(moves,' ', depth, ' ', time_taken)
-    delay = 1
+    print(moves, ' ', depth, ' ', time_taken)
 
+    delay = 1
     if moves > 30:
-        delay = 0.001
+        delay = 20 / moves
+
     puzzle.show_solution(moves, delay, path)
 
     time_taken = round(time_taken, 5)
-    witdth = (PUZZLE_WIDTH-(2*INBTWN_SPACE))//3
-    time_text = Button(SIDES_PADDING, 10, witdth, BUTTON_HEIGHT-60, RED," Time: "+str(time_taken)+ "s",BACK_GRND_COLOR,text_size-10)
+    witdth = (PUZZLE_WIDTH - (2 * INBTWN_SPACE)) // 3
+    time_text = Button(SIDES_PADDING, 10, witdth, BUTTON_HEIGHT - 60, RED, " Time: " + str(time_taken) + "s",
+                       BACK_GRND_COLOR, text_size - 10)
     time_text.draw(puzzle.screen)
-    Max_depth_text = Button(SIDES_PADDING+ witdth+INBTWN_SPACE, 10, witdth, BUTTON_HEIGHT-60, RED," Max depth: "+str(depth),BACK_GRND_COLOR,text_size-10)
+    Max_depth_text = Button(SIDES_PADDING + witdth + INBTWN_SPACE, 10, witdth, BUTTON_HEIGHT - 60, RED,
+                            " Max depth: " + str(depth), BACK_GRND_COLOR, text_size - 10)
     Max_depth_text.draw(puzzle.screen)
-    total_no_moves_text = Button(SIDES_PADDING+(2*witdth)+(2*INBTWN_SPACE), 10, witdth, BUTTON_HEIGHT-60, RED," # of explored: " + str(no_explored),BACK_GRND_COLOR,text_size-10)
+    total_no_moves_text = Button(SIDES_PADDING + (2 * witdth) + (2 * INBTWN_SPACE), 10, witdth, BUTTON_HEIGHT - 60, RED,
+                                 " # of explored: " + str(no_explored), BACK_GRND_COLOR, text_size - 10)
     total_no_moves_text.draw(puzzle.screen)
-    total_no_moves_text = Button(SIDES_PADDING, 10+INBTWN_SPACE+BUTTON_HEIGHT-60, witdth, BUTTON_HEIGHT-60, RED," Cost of path: " + str(moves),BACK_GRND_COLOR,text_size-10)
+    total_no_moves_text = Button(SIDES_PADDING, 10 + INBTWN_SPACE + BUTTON_HEIGHT - 60, witdth, BUTTON_HEIGHT - 60, RED,
+                                 " Cost of path: " + str(moves), BACK_GRND_COLOR, text_size - 10)
     total_no_moves_text.draw(puzzle.screen)
     pygame.display.update()
-
 
     running = True
     while running:
@@ -261,4 +280,3 @@ else:
             if event.type == pygame.QUIT:
                 running = False
     pygame.quit()
-
